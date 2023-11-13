a6b805f9ff8384ff6bb3de5c124cf6a15bac4aa5
Dedy Mirwansyah <110533746+ayaHMehru@users.noreply.github.com>
2023-11-13 14:02:08 +0800

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-
 import torch
 import torch.nn as nn
-from torch.utils.data import dataset, DataLoader
 from torchvision import transforms as T
 import torchvision
 import torch.nn.functional as F
-from torch.autograd import Variable
 
 from PIL import Image
 import cv2
